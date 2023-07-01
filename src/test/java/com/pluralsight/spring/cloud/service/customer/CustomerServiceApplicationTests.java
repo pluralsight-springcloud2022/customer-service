ff37381b54f5b8df4abbdd9572267c4d68656943
mhoffman <mike@michaelhoffmaninc.com>
2023-07-01 16:57:13 -0500

    Initial Commit

---
+++ src/test/java/com/pluralsight/spring/cloud/service/customer/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.spring.cloud.service.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
